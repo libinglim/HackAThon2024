0b9c2514440ebbea3f048cdd2a5b3b91556b1f6d
Platypus88 <79812179+Biped-Potato@users.noreply.github.com>
2024-03-02 14:15:17 -0600

    Add

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function handleClick() {
     alert('');
 }
 
-for(let i = 1;i<25;i++)
+for(let i = 1;i<31;i++)
 {
     var button = document.getElementById("Day"+i);
     button.addEventListener('click', handleClick);
